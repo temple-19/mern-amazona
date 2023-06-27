ad805edd9187866bfe846be2356d44dee74b18cc
temple_19 <tozoaz@gmail.com>
2023-06-27 14:24:22 +0100

    hhhh

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ import userRouter from './routes/userRoutes.js';
 import orderRouter from './routes/orderRoutes.js';
 import cors from 'cors';
 
+const app = express();
 dotenv.config();
 mongoose
   .connect(process.env.MONGODB_URI)
@@ -19,7 +20,6 @@ mongoose
     err.message;
   });
 
-const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
@@ -151,7 +151,7 @@ app.use('/api/orders', orderRouter);
 // app.use(express.static(path.join(__dirname, '/frontend/build')));
 // app.get('*', (req, res) =>
 //   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
-// );
+// )
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
